03d451ede5306a59585400236d8f03f25a47d8b4
VladyThrash <vladyepcon@gmail.com>
2026-06-11 09:24:23 -0600

    Añadiendo merge_sort. Si, voy muy lento :)

--- Source_Code/Message_Analysis.h
+++ Source_Code/Message_Analysis.h
@@ -14,12 +14,52 @@ Módulo 1:
 #include<string.h>
 #include "Leer_Numero.h" //Es solo una función de lectura segura de números enteros.
 
+//Struct para vaciar la información del archivo.
+struct mensaje{
+	char id[20];
+    char remitente[50];
+    char prioridad[10];
+    char fecha[20];
+    char clave_id[20];
+    char texto_original[256];
+    char texto_cifrado[256];
+};
+
+//Path al archivo txt.
+char path[] = "Cryptonituv_DB/mensajes_cifrados.txt";
+
+//Puntero para crear arreglo de structs
+struct mensaje *mensajes = NULL;
+
 //Toma el control del flujo del programa desde main.
 void message_analysis_menu();
 
+//Inicializa la lectura del archivo txt y extrae la información en un struct.
+int extraer_txt();
+
+//Imprimir todos los registros obtenidos en el arreglo de structs.
+void imprimir_registros(int n);
+
+//Ordena en orden ASC los mensajes basado en las fechas  formato ISO: AAAA-MM-DD.
+//Aplica strcmp para obtener directamente el valor lexicografico.
+//Complejidad: O(n*log(n)). Requiere generar memoria de manera dinamica.
+void merge_sort(struct mensaje *arr, int izq, int der);
+void merge(struct mensaje *arr, int izq, int mid, int der);
+
+//Para copiar el indice de un arreglo de structs en otro.
+void copy(struct mensaje *dst, struct mensaje *src);
+
+//Para hacer swap entre indices del arreglo de structs.
+void swap(struct mensaje *msg1, struct mensaje *msg2);
+
 
 void message_analysis_menu(){
-    int input = 0;
+	int num_registros = extraer_txt();
+    if(num_registros == 0){ //Validar que se extrajo correctamente la información del txt.
+		return;
+	}
+
+	int input = 0;
     do{
         printf("\n--- LISTAR MENSAJES CIFRADOS EN ---\n");
         printf("[1]. Prioridad orden DESC...\n");
@@ -32,19 +72,24 @@ void message_analysis_menu(){
 
         switch(input){
             case 1:
-                
+                printf("\n--- LISTADO EN PRIORIDAD ORDEN DESC ---\n");
+                imprimir_registros(num_registros);
                 break;
 
             case 2:
-                
+                merge_sort(mensajes, 0, num_registros - 1); //Ordenar por fecha ASC.
+                printf("\n--- LISTADO EN FECHA ORDEN ASC ---\n");
+                imprimir_registros(num_registros);
                 break;
 
             case 3:
-                
+                printf("\n--- LISTADO EN REMITENTE ORDEN ASC ---\n");
+                imprimir_registros(num_registros);
                 break;
 
             case 4:
-                
+                printf("\n--- LISTADO EN LONGITUD MENSAJE ORDEN ASC ---\n");
+                imprimir_registros(num_registros);
                 break;
 
             case 0:
@@ -57,6 +102,178 @@ void message_analysis_menu(){
         } 
     }
     while(input);
+	free(mensajes); //Liberar memoria del arreglo de structs.
+}
+
+int extraer_txt(){
+	//Abrimos el archivo en modo lectura ("r").
+    FILE *archivo = fopen(path, "r");
+    if (archivo == NULL) {
+        printf("ERROR: NO SE PUDO ABRIR EL ARCHIVO...\n");
+        return 0;
+    }
+
+	char buffer[1024];
+    int total_lineas = 0; //Número total de registros.
+
+	//Leer y descartar la primera linea
+    if (fgets(buffer, sizeof(buffer), archivo) != NULL) {
+        while (fgets(buffer, sizeof(buffer), archivo) != NULL) {
+            total_lineas++; //Contar el total de registros.
+        }
+    }
+
+	if(total_lineas == 0){
+        printf("EL ARCHIVO NO CONTIENE DATOS...\n");
+        fclose(archivo);
+        return 0;
+    }
+
+	//Crear el arreglo con el número total de registros.
+    mensajes = (struct mensaje*)malloc(sizeof(struct mensaje)*total_lineas);
+    if(mensajes == NULL){
+        printf("ERROR: NO SE PUDO ASIGNAR MEMORIA...\n");
+        fclose(archivo);
+        return 0;
+    }
+
+	rewind(archivo); //Regresar el archivo al inicio.
+    fgets(buffer, sizeof(buffer), archivo); //Descartar el encabezado.
+
+	//Leer linea por linea y asignar a cada indice del arreglo los campos correspondientes.
+	for(int i = 0; fgets(buffer, sizeof(buffer), archivo) != NULL && i < total_lineas; i++){
+		char *token = strtok(buffer, "|\n");
+        if (token != NULL) {
+			strcpy(mensajes[i].id, token);
+		}
+
+        token = strtok(NULL, "|\n");
+        if (token != NULL) {
+			strcpy(mensajes[i].remitente, token);
+		}
+
+        token = strtok(NULL, "|\n");
+        if (token != NULL) {
+			strcpy(mensajes[i].prioridad, token);
+		}
+
+        token = strtok(NULL, "|\n");
+        if (token != NULL) {
+			strcpy(mensajes[i].fecha, token);
+		}
+
+        token = strtok(NULL, "|\n");
+        if (token != NULL) {
+			strcpy(mensajes[i].clave_id, token);
+		}
+
+        token = strtok(NULL, "|\n");
+        if (token != NULL) {
+			strcpy(mensajes[i].texto_original, token);
+		}
+
+        token = strtok(NULL, "|\n");
+        if (token != NULL) {
+			strcpy(mensajes[i].texto_cifrado, token);
+		}
+	}
+
+	fclose(archivo); //Cerrar el archivo.
+	return total_lineas;
+}
+
+void imprimir_registros(int n){
+	printf("ID\t|\tRemitente\t|\tPrioridad\t|\tFecha\t|\tClave ID\t|\tTexto Cifrado\n");
+	printf("-----------------------------------------------------------------------------------------------------------------------------\n");
+	
+	for(int i = 0; i < n; i++){
+		printf("%s\t|\t%s\t\t|\t%s\t|\t%s\t|\t%s\t|\t%s\n", mensajes[i].id, mensajes[i].remitente, mensajes[i].prioridad,
+													mensajes[i].fecha, mensajes[i].clave_id, mensajes[i].texto_cifrado);
+	}
+	printf("-----------------------------------------------------------------------------------------------------------------------------\n");
+}
+
+void merge_sort(struct mensaje *arr, int izq, int der){
+    if(izq < der){
+        int mid = (izq + der)/2;
+        merge_sort(arr, izq, mid); //sub_arr izquierdo
+        merge_sort(arr, mid+1, der); //sub_arr derecho
+        merge(arr, izq, mid, der);
+    }
+}
+
+void merge(struct mensaje *arr, int izq, int mid, int der){
+    int tam = der - izq + 1; //Tamaño del subarreglo mezclado.
+    struct mensaje *aux = (struct mensaje*)malloc(sizeof(struct mensaje)*tam); //Arreglo auxiliar.
+    int i_der = mid + 1; //Para iterar sobre el subarreglo derecho.
+    int izq_aux = izq; //Para iterar sobre el subarreglo izquierdo.
+    for(int i = 0; i < tam; i++){
+        if(izq > mid){ //Ya iteramos todo el subarreglo izquierdo, vaciamos lo que queda del derecho.
+            copy(&aux[i], &arr[i_der]);
+            i_der++;
+            continue;
+        }
+        if(i_der > der){ //Ya iteramos todo el subarreglo derecho, vaciamos lo que queda del izquierdo.
+            copy(&aux[i], &arr[izq]);
+            izq++;
+            continue;
+        }
+        //strcmp(cad1, cad2): devuelve 0 si cad1 == cad2, negativo si cad1 < cad2, positivo si cad1 > cad2.
+        if(strcmp(arr[izq].fecha, arr[i_der].fecha) < 0){
+            copy(&aux[i], &arr[izq]); //El valor en el subarreglo izquierdo es menor, copiamos en aux.
+            izq++;
+        }
+        else{
+            copy(&aux[i], &arr[i_der]); //El valor en el subarreglo derecho es menor, copiamos en aux.
+            i_der++;
+        }
+    }
+    for(int i = 0; i < tam; i++, izq_aux++){
+        copy(&arr[izq_aux], &aux[i]); //Vaciamos aux ordenado en el subarreglo mezclado.
+    }
+    free(aux);
+}
+
+void copy(struct mensaje *dst, struct mensaje *src){
+    //Copiamos el contenido de src en dst.
+    strcpy(dst->id, src->id);
+    strcpy(dst->remitente, src->remitente);
+    strcpy(dst->prioridad, src->prioridad);
+    strcpy(dst->fecha, src->fecha);
+    strcpy(dst->clave_id, src->clave_id);
+    strcpy(dst->texto_original, src->texto_original);
+    strcpy(dst->texto_cifrado, src->texto_cifrado);
+}
+
+void swap(struct mensaje *msg1, struct mensaje *msg2){
+    struct mensaje aux;
+
+    //Copiamos contenido de msg1 a aux.
+    strcpy(aux.id, msg1->id);
+    strcpy(aux.remitente, msg1->remitente);
+    strcpy(aux.prioridad, msg1->prioridad);
+    strcpy(aux.fecha, msg1->fecha);
+    strcpy(aux.clave_id, msg1->clave_id);
+    strcpy(aux.texto_original, msg1->texto_original);
+    strcpy(aux.texto_cifrado, msg1->texto_cifrado);
+
+    //Copiamos contenido de msg2 a msg1.
+    strcpy(msg1->id, msg2->id);
+    strcpy(msg1->remitente, msg2->remitente);
+    strcpy(msg1->prioridad, msg2->prioridad);
+    strcpy(msg1->fecha, msg2->fecha);
+    strcpy(msg1->clave_id, msg2->clave_id);
+    strcpy(msg1->texto_original, msg2->texto_original);
+    strcpy(msg1->texto_cifrado, msg2->texto_cifrado);
+
+    //Copiamos contenido de aux a msg2.
+    strcpy(msg2->id, aux.id);
+    strcpy(msg2->remitente, aux.remitente);
+    strcpy(msg2->prioridad, aux.prioridad);
+    strcpy(msg2->fecha, aux.fecha);
+    strcpy(msg2->clave_id, aux.clave_id);
+    strcpy(msg2->texto_original, aux.texto_original);
+    strcpy(msg2->texto_cifrado, aux.texto_cifrado);
 }
 
 #endif
